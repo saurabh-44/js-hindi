e90dea975c122eb9c2e10340a26a1c44249db965
Saurabh yadav <938saurabh@gmail.com>
2024-09-23 14:42:53 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,16 @@
 const accountId = 144553
+// accountId = 2 // not allowed we can't cahnge const
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
-let accountState;
+let accountState;    // undefined
 
-// accountId = 2 // not allowed
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 
 
 accountEmail = "hc@hc.com"
@@ -13,10 +19,5 @@ accountCity = "Bengaluru"
 
 console.log(accountId);
 
-/*
-Prefer not to use var
-because of issue in block scope and functional scope
-*/
-
-
+// will print in table formate
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

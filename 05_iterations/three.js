d25c6a40b96323b4ff4fa8f28a8ec66dd911b5db
Saurabh yadav <938saurabh@gmail.com>
2024-09-23 10:45:19 +0530

    Update three.js

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,29 +1,26 @@
-// for of
+// for of => to iterate  of object, array
 
 // ["", "", ""]
 // [{}, {}, {}]
 
 const arr = [1, 2, 3, 4, 5]
-
-for (const num of arr) {
-    //console.log(num);
+for (const num of arr) {    // arr indicates that where we want to perform 
+    console.log(num);
 }
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+    console.log(`Each char is ${greet}`)
 }
 
-// Maps
-
+// Maps => object which holds key-value pairs and remembers the original insertion order of the keys.
 const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
-map.set('IN', "India")
+map.set('IN', "India")    // unique entries will there with order
 
-
-// console.log(map);
+console.log(map);
 
 for (const [key, value] of map) {
     // console.log(key, ':-', value);
@@ -34,7 +31,6 @@ const myObject = {
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
+// for (const [key, value] of myObject) {       object is not iteratable
 //     console.log(key, ':-', value);
-    
 // }

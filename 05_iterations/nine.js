b87a803da52ffd2ba2281b20ecf264927c1d5051
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 17:25:30 +0530

    Update nine.js

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,14 +1,19 @@
+/*-----------------------------------Reduce----------------------------------------
+=> an array method that processes each element of the array and reduces it to a single value. 
+=> iterates over the array, and for each element, it applies the callback function. 
+=> The result from the callback is stored in an accumulator that is carried forward to the next iteration. Finally, the method returns the accumulated value.*/
 const myNums = [1, 2, 3]
 const myTotal = myNums.reduce(function (acc, currval) {
     console.log(`acc: ${acc} and currval: ${currval}`);
     return acc + currval
 }, 0)    // 0 is the initial value of accumulator 
 
-// Arrow function
+/*-----------------------------with arrow function----------------------*/
 const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 console.log(myTotal);
 
 
+/*--------------------------------Example------------------------------*/
 const shoppingCart = [
     {
         itemName: "js course",
@@ -27,6 +32,5 @@ const shoppingCart = [
         price: 12999
     },
 ]
-
 const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 console.log(priceToPay);

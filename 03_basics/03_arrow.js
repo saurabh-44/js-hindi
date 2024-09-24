e11e176053c6f0bb20ec539c06182e6b615a087b
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 11:39:34 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,54 +1,52 @@
+/*-------------------------------this keyword------------------------------*/
 const user = {
     username: "hitesh",
     price: 999,
-
-    welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+    welcomeMessage: function() {   // can initialize function inside the object
+        console.log(`${this.username} , welcome to website`);  // with this keyword we can access attribute within an object
+        console.log(this);         // this will print entire object  
     }
-
 }
+user.welcomeMessage()       // call the function 
+user.username = "sam"       // username will update here
+user.welcomeMessage()       // updated username will call
+console.log(this);          // updated object will print
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
 
-// console.log(this);
+/*-------------------------------Arrow Function------------------------------*/
+// 1st method of decalaration function
+function chai(){
+    let username = "hitesh"
+    console.log(this.username);
+}
+chai()
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
 
-// chai()
+// 2nd method of decalaration function
+const chai = function () {
+    let username = "hitesh"
+    console.log(this.username);
+}
+chai()
 
-// const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
-
+// 3rd method of decalaration function with arrow function which is the shortest way of writing any function
 const chai =  () => {
     let username = "hitesh"
     console.log(this);
 }
+chai()
 
 
-// chai()
+/*-------------------------------Arrow Function Exercises------------------------------*/
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
-
-// const addTwo = (num1, num2) =>  num1 + num2
-
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
-const addTwo = (num1, num2) => ({username: "hitesh"})
-
-
+// or inline method
+const addTwo = (num1, num2) =>  num1 + num2
+// or
+const addTwo = (num1, num2) => ( num1 + num2 )
 console.log(addTwo(3, 4))
 
-
-// const myArray = [2, 5, 3, 7, 8]
-
-// myArray.forEach()
+// object creation
+const addTwo = (num1, num2) => ({username: "hitesh"})

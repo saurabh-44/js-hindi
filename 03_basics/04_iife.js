8baefd083389d149c0f9e4c5dce6d91a4b598b6a
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 11:46:27 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,13 @@
-// Immediately Invoked Function Expressions (IIFE)
-
+// Immediately Invoked Function Expressions (IIFE) =>  a JavaScript function that runs as soon as it is defined.
+//  If we have some initiation code that we don't need to use again, we could use the IIFE pattern.
+//  () => anonymous function with lexical scope enclosed within the grouping operator (). This prevents accessing variables within the IIFE idiom and polluting the global scope.
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();     // () => immediately invoked function expression () through which the JavaScript engine will directly interpret the function.
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('hitesh')  // hitesh will print after $ sign
 

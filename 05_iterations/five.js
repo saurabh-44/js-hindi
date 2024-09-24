d7e6bf3f995f9f1aafd2ba93583b086f21a6af4d
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 16:35:25 +0530

    Update five.js

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,22 +1,33 @@
+/*------------------------------------forEach------------------------------------------
+=> is an array method that allows you to execute a function for each element in an array.
+=> simply iterates through the array and performs the action inside the callback. It does not create a new array or return any value.
+=> cannot use break or continue inside forEach(). It will always run for every element.
+=> If the callback modifies the elements of the array, those changes will be reflected in the original array.
+=> If you want to create a new array with modified values, use map() instead. forEach() is primarily for iterating and performing actions, not transforming data. */
 const coding = ["js", "ruby", "java", "python", "cpp"]
-
 coding.forEach( function (val){         // callback function is an parameters
     console.log(val);
 } )
 
-coding.forEach( (item) => {
+coding.forEach( (item) => {             // with arrow function
     console.log(item);
 } )
 
+
+// 2nd method
 function printMe(item){
     console.log(item);
 }
 coding.forEach(printMe)
 
+/*------------------------------------forEach with index----------------------------------*/
+const coding1 = ["js", "ruby", "java", "python", "cpp"]
 coding.forEach( (item, index, arr)=> {
-    console.log(item, index, arr);
+    console.log(item, index, arr);        // here item, index and entire array will print
 } )
 
+
+/*------------------------------------forEach with object----------------------------------*/
 const myCoding = [
     {
         languageName: "javascript",

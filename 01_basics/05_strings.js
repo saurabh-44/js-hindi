e955689007cab1a6528b1afe67e8a972d0368950
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 19:00:04 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,35 +1,35 @@
-const name = "hitesh"
+/*--------------------------------------------strings-----------------------------------------------
+=> A string is a sequence of characters used to represent text. In JavaScript, strings are immutable (once created, they cannot be changed) 
+=> can be defined using single quotes ('), double quotes ("), or template literals (`).*/
+const name = "saurabh"
 const repoCount = 50
-
-// console.log(name + repoCount + " Value");
-
+console.log(name + repoCount + " Value");
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+/*--------------------------------------------Prototypes-----------------------------------------------*/
+const gameName = new String('saurabh-sy-com')  // 2nd method to declaration
+console.log(gameName[0]);
+console.log(gameName.__proto__);              // in browser console there are so many prototypes in the string
 
-
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
+console.log(gameName.length);                
+console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
+console.log(gameName.split('-'));
 
-const newString = gameName.substring(0, 4)
+
+/*--------------------------------------------Example-----------------------------------------------*/
+const newString = gameName.substring(0, 4)   // return index from 0 to 3 
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-4, -2)  // return -c => 4th element from last to 2+1 element from last
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   saurabh    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());         // remove spaces from start
 
-const url = "https://hitesh.com/hitesh%20choudhary"
-
-console.log(url.replace('%20', '-'))
-
-console.log(url.includes('sundar'))
-
-console.log(gameName.split('-'));
+const url = "https://saurabh.com/saurabh%20yadav"           
+console.log(url.replace('%20', '-'))                 // https://saurabh.com/saurabh-yadav 
+console.log(url.includes('sundar'))                  // it checks whether the substring it present in main string or not

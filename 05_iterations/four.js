35ebf8056ee3fc928244f926b76518f43730ca71
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 16:06:20 +0530

    Update four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,25 +1,31 @@
+/*---------------------------------for...in-------------------------------------
+=> The for...in loop is used to iterate over the enumerable properties (keys) of an object and index of an array. 
+=> Best suited for objects when you want to loop through their keys.            */
 const myObject = {
     js: 'javascript',
     cpp: 'C++',
     rb: "ruby",
     swift: "swift by apple"
 }
-
 for (const key in myObject) {
     console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
+
+/*-------------------------------for...in with array-----------------------------------*/
 const programming = ["js", "rb", "py", "java", "cpp"]
 for (const key in programming) {
     console.log(programming[key]);
 }
 
-// const map = new Map()      => is not iteratable
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
 
-// for (const key in map) {
-//     console.log(key);
+/*-------------------------------for...in with Map object---------------------------------*/
+const map = new Map()      // is not iteratable with Map object
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key);
 // }

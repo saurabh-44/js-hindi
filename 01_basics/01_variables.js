6cc6bc8b736d7a6f2840d39508b9f7e86b385ea3
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 17:40:53 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,23 +1,26 @@
-const accountId = 144553
-// accountId = 2 // not allowed we can't cahnge const
+"use strict"; // treat all JS code as newer version
+
+console.log("Hello bro");    // automatically new line come after printing
+console.log(3+4);            // addition => 7
+console.log("3+4",3,"4"+5);  // 3+4 3 45
+
+let a = 8;
+console.log(`the value of a is ${a}`);
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+
+const accountId = 144553        // once we declare variable with const then we can't change the value again
 let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
+var accountPassword = "12345"   // Prefer not to use var because of issue in block scope and functional scope
 accountCity = "Jaipur"
 let accountState;    // undefined
 
-
-/*
-Prefer not to use var
-because of issue in block scope and functional scope
-*/
-
-
-
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
-accountCity = "Bengaluru"
-
-console.log(accountId);
+accountEmail = "hc@hc.com"      // can redeclare 
+accountPassword = "21212121"    // can redeclare
+accountCity = "Bengaluru"       // can redeclare
 
 // will print in table formate
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

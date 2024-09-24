c3c460cf58932d0ba687617fa9b6743d36f9f55a
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 11:22:51 +0530

    Update 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,58 +1,51 @@
-//var c = 300
-let a = 300
+/*----------------------------- var, let & const--------------------------*/
+let a = 300      // global scope
+var c = 300
 if (true) {
-    let a = 10
+    let a = 10   // lexical scope  
     const b = 20
-    // console.log("INNER: ", a);
+    var c = 40
+    console.log("INNER: ", a);
     
 }
+console.log(a);   // we are using let so here we can't access the value of inside the scope so 300 will print 
+console.log(b);   // b is not defined globally 
+console.log(c);   // here 40 will print which creates the problem because of var and we can access the local value to the outside the scope which is not good
 
 
-
-// console.log(a);
-// console.log(b);
-// console.log(c);
-
-
+/*----------------------------- Example => Scope --------------------------*/
 function one(){
     const username = "hitesh"
-
     function two(){
         const website = "youtube"
         console.log(username);
     }
-    // console.log(website);
-
-     two()
-
+    console.log(website);    // website is not defined globally so we can't access outside the scope
+    two()                    // if we comment out upper line then it will execute and username will print   
 }
+one()
 
-// one()
 
+/*----------------------------- Example => Scope --------------------------*/
 if (true) {
     const username = "hitesh"
     if (username === "hitesh") {
         const website = " youtube"
-        // console.log(username + website);
+        console.log(username + website);
     }
-    // console.log(website);
+    console.log(website);  // No access of website because of lexical scope
 }
-
-// console.log(username);
-
-
-// ++++++++++++++++++ interesting ++++++++++++++++++
+console.log(username);     // No access of username because of lexical scope
 
 
-console.log(addone(5))
-
+/*----------------------------- Interesting-1 --------------------------*/
+console.log(addone(5))   // 6 will print no issue to call before declaration of function 
 function addone(num){
     return num + 1
 }
 
-
-
-addTwo(5)
-const addTwo = function(num){
+/*----------------------------- Interesting-2 --------------------------*/
+console.log(addTwo(5))          // Here problem will create because addTwo is the variable and we can't access before initialization. 
+const addTwo = function(num){   // 2nd method to declaration of function
     return num + 2
 }

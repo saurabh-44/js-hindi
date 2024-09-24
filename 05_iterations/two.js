34155ab9e534f62ca7e15b4b97ca19c75bc0d71d
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 15:19:09 +0530

    Update two.js

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,20 +1,22 @@
-
+/*----------------------------------while loop----------------------------*/
 let index = 0
-// while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
-// }
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
 
+
+/*----------------------------------Exmaple-1----------------------------*/
 let myArray = ['flash', "batman", "superman"]
-
 let arr = 0
 while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
+    console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1
 }
 
-let score = 11
 
+/*---------------------------------do while loop--------------------------*/
+let score = 11
 do {
     console.log(`Score is ${score}`);
     score++

9d61f1d169858d4c7dcf0763e54d5abe37593d6c
Saurabh yadav <938saurabh@gmail.com>
2024-09-24 13:22:16 +0530

    Update switch.js

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,18 +1,17 @@
-// switch (key) {
-//     case value:
-        
-//         break;
+/*        switch (key) {
+            case value:
+                break;
 
-//     default:
-//         break;
-// }
+            default:
+                break;
+}                                    */
 
+/*-----------------------------------Exmaple----------------------------------------*/
 const month = "march"
-
-switch (month) {
-    case "jan":
+switch (month) {    // we can give any kind of key where it is num string or anything
+    case "jan":     // case should be same as key formate
         console.log("January");
-        break;
+        break;      // after matching the case it break break terminate the switch code
     case "feb":
         console.log("feb");
         break;
@@ -23,7 +22,7 @@ switch (month) {
         console.log("april");
         break;
 
-    default:
+    default:         // when any of the case will not match then it will execute default
         console.log("default case match");
         break;
 }

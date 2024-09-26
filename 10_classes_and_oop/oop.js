d818fece58d8b7a06c300499f6fdc140f4d58a99
Saurabh yadav <938saurabh@gmail.com>
2024-09-26 13:39:16 +0530

    Update oop.js

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,23 +1,25 @@
+/*--------------------------------------------oops----------------------------------------------------*/
+// JavaScript does have classes and it was introduce with ES6. and it is a prototype-based language. it's not a object oriented programming.
 const user = {
     username: "hitesh",
     loginCount: 8,
     signedIn: true,
 
     getUserDetails: function(){
-        //console.log("Got user details from database");
-        // console.log(`Username: ${this.username}`);
-        console.log(this);
+        console.log("Got user details from database");
+        console.log(`Username: ${this.username}`);            // directly didn't understand which username so .this will refers to the user object
+        console.log(this);                                    // this will refers to current context means current object
     }
 
 }
+console.log(user.username)        // hitesh
+console.log(user.getUserDetails());   // object
+console.log(this);                     // it's refers to the globel context or object then output will be {} empty object. and it we check this line of code
+// on browser console then it will refers to the window object which is globel object
 
 
-
-//console.log(user.username)
-//console.log(user.getUserDetails());
-// console.log(this);
-
-
+/*-------------------------------------------constructor function-------------------------------------------
+=> it will give you a copy in which chages will be yours and do not override */
 function User(username, loginCount, isLoggedIn){
     this.username = username;
     this.loginCount = loginCount;
@@ -25,13 +27,13 @@ function User(username, loginCount, isLoggedIn){
 
     this.greeting = function(){
         console.log(`Welcome ${this.username}`);
-
     }
-
     return this
 }
+// const userOne =  User("hitesh", 12, true)
+// const userTwo =  User("ChaiAurCode", 11, false)
+//console.log(userOne);     => if we execute over two line then userOne will update with userTwo because of override
 
 const userOne = new User("hitesh", 12, true)
 const userTwo = new User("ChaiAurCode", 11, false)
 console.log(userOne.constructor);
-//console.log(userTwo);

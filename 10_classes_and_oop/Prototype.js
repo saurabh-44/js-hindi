fb7873aae99a2e79df956e30722291a447adc2c1
Saurabh yadav <938saurabh@gmail.com>
2024-09-27 19:37:09 +0530

    Update Prototype.js

--- 10_classes_and_oop/Prototype.js
+++ 10_classes_and_oop/Prototype.js
@@ -2,22 +2,20 @@
 // let myName = "hitesh     "
 // let mychannel = "chai     "
 
-// console.log(myName.trueLength);
+// console.log(myName.trueLength);              // we want a method which removes white spaces and return the length of the string with one method.
 
 
+/*-----------------------------we are injecting a method to object which fuction can also access*/
 let myHeros = ["thor", "spiderman"]
-
-
 let heroPower = {
     thor: "hammer",
     spiderman: "sling",
-
     getSpiderPower: function(){
         console.log(`Spidy power is ${this.spiderman}`);
     }
 }
 
-Object.prototype.hitesh = function(){
+Object.prototype.hitesh = function(){                       // initializing prototype method adding new property to object
     console.log(`hitesh is present in all objects`);
 }
 
@@ -25,13 +23,14 @@ Array.prototype.heyHitesh = function(){
     console.log(`Hitesh says hello`);
 }
 
-// heroPower.hitesh()
-// myHeros.hitesh()
-// myHeros.heyHitesh()
-// heroPower.heyHitesh()
+// Now can access the new prototype method which we initialize previously
+heroPower.hitesh()
+myHeros.hitesh()
+myHeros.heyHitesh()
+heroPower.heyHitesh()   // we give power to array not an object.
 
-// inheritance
 
+// inheritance => old syntax
 const User = {
     name: "chai",
     email: "chai@google.com"
@@ -48,18 +47,20 @@ const TeachingSupport = {
 const TASupport = {
     makeAssignment: 'JS assignment',
     fullTime: true,
-    __proto__: TeachingSupport
+    __proto__: TeachingSupport    // reference to TeachingSupport and it can access
 }
 
-Teacher.__proto__ = User
+Teacher.__proto__ = User          // teacher can access users by this property and it is old syntax
+
 
 // modern syntax
-Object.setPrototypeOf(TeachingSupport, Teacher)
+Object.setPrototypeOf(TeachingSupport, Teacher)     // which protperty is going to injected to which property
 
+
+/*-------------------------------------------------------------Problem -1st-----------------------------------------*/
 let anotherUsername = "ChaiAurCode     "
-
 String.prototype.trueLength = function(){
-    console.log(`${this}`);
+    console.log(`${this}`);                                            // chai aur code
     console.log(`True length is: ${this.trim().length}`);
 }
 

5f072767e2955c25ae89894feb8c6706f0a0e9c9
Saurabh yadav <938saurabh@gmail.com>
2024-09-28 11:51:29 +0530

    Update object_get_set.js

--- 10_classes_and_oop/object_get_set.js
+++ 10_classes_and_oop/object_get_set.js
@@ -1,10 +1,10 @@
+// object based get set
 const User = {
     _email: 'h@hc.com',
     _password: "abc",
 
-
     get email(){
-        return this._email.toUpperCase()
+        return this._email.toUpperCase()         // if we write only email then it will be race situation of calls
     },
 
     set email(value){
@@ -12,5 +12,5 @@ const User = {
     }
 }
 
-const tea = Object.create(User)
+const tea = Object.create(User) //factory function we can make object based on user and reger to tea
 console.log(tea.email);

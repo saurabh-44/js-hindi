5d8c0843d66164c3173b433b39d90679aa5f33ed
Saurabh yadav <938saurabh@gmail.com>
2024-09-29 14:14:47 +0530

    Update object_get_set.js

--- 10_classes_and_oop/object_get_set.js
+++ 10_classes_and_oop/object_get_set.js
@@ -1,16 +1,17 @@
 // object based get set
 const User = {
-    _email: 'h@hc.com',
+    _email: 'h@hc.com',                          // Private-like property to store the email (underscore is a convention for "private" properties)
     _password: "abc",
 
     get email(){
         return this._email.toUpperCase()         // if we write only email then it will be race situation of calls
     },
 
-    set email(value){
-        this._email = value
+    set email(value){ 
+        this._email = value                      // Updates the '_email' field with the new value
     }
 }
 
-const tea = Object.create(User) //factory function we can make object based on user and reger to tea
+const tea = Object.create(User)                  // Creates a new object 'tea' that inherits from 'User'
+//factory function we can make object based on user and refers to tea
 console.log(tea.email);

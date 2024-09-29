1f9f99f532d7cc2edfc4a8fbd5a146c683d01155
Saurabh yadav <938saurabh@gmail.com>
2024-09-29 12:11:03 +0530

    Update staticprop.js

--- 10_classes_and_oop/staticprop.js
+++ 10_classes_and_oop/staticprop.js
@@ -1,26 +1,32 @@
+/* 7---------------inheritance, method overriding, and static methods in JavaScript.----------------*/
 class User {
     constructor(username){
-        this.username = username
+        this.username = username        // The constructor initializes the username property
     }
-
-    logMe(){
+    logMe(){                            // Instance method that logs the username to the console
         console.log(`Username: ${this.username}`);
     }
-
-    static createId(){
-        return `123`
+    static createId(){                  // Static method => This is a static method, which means it belongs to the class itself and not to individual instances.
+        return `123`                    // Returns a static ID
     }
 }
 
-const hitesh = new User("hitesh")
+const saurabh = new User("saurabh")     /* If you try to call saurabh.createId(), it will throw an error because createId is a static method 
+and cannot be accessed through an instance (saurabh). The correct way to call a static method is User.createId(), directly from the class.*/
 // console.log(hitesh.createId())
 
-class Teacher extends User {
+class Teacher extends User {            // Teacher class has access to all properties and methods of the User class.
     constructor(username, email){
-        super(username)
-        this.email = email
+        super(username)                 // Calls the parent class (User) constructor to initialize the username
+        this.email = email              // Initializes an additional property (email) for Teacher
     }
 }
+const iphone = new Teacher("iphone", "i@phone.com")     // Teacher inherits from User, the iphone object can access methods from the User class.
+console.log(iphone.createId());      /* This will throw an error because createId() is a static method of the User class and cannot be accessed via an instance 
+of the class (iphone). Static methods can only be called directly on the class itself 
+(e.g., User.createId() or Teacher.createId() if Teacher had its own static method or inherited one).*/
 
-const iphone = new Teacher("iphone", "i@phone.com")
-console.log(iphone.createId());
+console.log(User.createId());     // Output: 123 => To correctly access the createId() static method, you should call it like this.
+console.log(Teacher.createId());  // Output: 123 => If you want to add or use a static method in the Teacher class, you would do the same.
+
+
